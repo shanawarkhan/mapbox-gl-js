e1a443c92a6d24b9c62153dc37379b0b6248f880
John Firebaugh <john.firebaugh@gmail.com>
2015-03-31 16:01:48 -0700

    Update to latest documentation code

--- docs/_generate/generate.js
+++ docs/_generate/generate.js
@@ -2,71 +2,33 @@
 
 var fs = require('fs'),
     path = require('path'),
-    marked = require('marked'),
-    _ = require('underscore'),
     documentation = require('documentation'),
     normalize = require('documentation/streams/normalize'),
+    html = require('documentation/streams/html'),
     flatten = require('documentation/streams/flatten'),
+    filterAccess = require('documentation/streams/filter_access'),
+    hierarchy = require('documentation/streams/hierarchy'),
     through = require('through'),
     Handlebars = require('handlebars');
 
-var renderer = new marked.Renderer(),
-    heading = {},
-    classes = {},
-    toc = '',
-    api = '';
+Handlebars.registerHelper('join', function(array, sep, options) {
+    return array ? array.map(function(item) { return options.fn(item); }).join(sep) : '';
+});
 
-function getClassDescriptor(name) {
-    var descriptor = classes[name]
+Handlebars.registerPartial('methods', fs.readFileSync(path.join(__dirname, './methods.html.hbs'), 'utf8'));
+Handlebars.registerPartial('signature', fs.readFileSync(path.join(__dirname, './signature.html.hbs'), 'utf8'));
 
-    if (!descriptor) {
-        descriptor = classes[name] = {
-            name: name,
-            staticMethods: [],
-            instanceMethods: []
-        };
-    }
-
-    return descriptor;
-}
-
-function document(data) {
-    if (data.kind === 'class' || data.kind === 'mixin') {
-        var descriptor = getClassDescriptor(data.name);
-        descriptor.description = data.classdesc;
-        descriptor.constructor = data;
-    }
-
-    else if (data.memberof) {
-        var descriptor = getClassDescriptor(data.memberof);
-        if (data.scope === 'instance') {
-            descriptor.instanceMethods.push(data);
-        } else if (data.scope === 'static') {
-            descriptor.staticMethods.push(data);
-        }
-    }
-}
+var template = Handlebars.compile(fs.readFileSync(path.join(__dirname, './index.html.hbs'), 'utf8'));
 
 documentation(require('../../package.json').main)
     .pipe(normalize())
+    .pipe(html())
     .pipe(flatten())
-    .pipe(through(document))
-    .on('error', function(err) { throw err; })
-    .on('end', function() {
-        Handlebars.registerHelper('join', function(array, sep, options) {
-            return array ? array.map(function(item) { return options.fn(item); }).join(sep) : '';
-        });
-
-        Handlebars.registerPartial('methods', fs.readFileSync(path.join(__dirname, './methods.html.hbs'), 'utf8'));
-        Handlebars.registerPartial('signature', fs.readFileSync(path.join(__dirname, './signature.html.hbs'), 'utf8'));
-
-        var template = Handlebars.compile(fs.readFileSync(path.join(__dirname, './index.html.hbs'), 'utf8'));
-
+    .pipe(filterAccess())
+    .pipe(hierarchy())
+    .pipe(through(function (classes) {
+        debugger;
         fs.writeFileSync(
             path.join(__dirname, '../_posts/3400-01-01-api.html'),
-            template({classes: _.values(classes)}));
-    });
-
-//renderer.code = function (code, lang) {
-//    return '{% highlight ' + lang + ' %}' + code + '\n{% endhighlight %}\n';
-//}
+            template({classes: classes}));
+    }));
